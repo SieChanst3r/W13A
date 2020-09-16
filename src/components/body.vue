5bc4633edf692d71fb91019d4e2119850cea5d65
SieChan <sie.chan@outlook.com>
2020-09-15 21:04:17 -0600

    test

---
+++ src/components/body.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="page-body">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:"page-body"
+}
+</script>
+
+<style scoped>
+ .page-body {
+        display:grid;
+        justify-items: center;
+        align-items: center;
+        grid-template-columns: 1fr 1fr 1fr;
+        height: 45vh;
+        border: solid black 2px; 
+    }
+</style>
